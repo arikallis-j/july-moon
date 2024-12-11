61741cba31d16adc3bbe7a617aaf8735f686e47d
arikallis-j <arikallis.j@gmail.com>
2024-12-11 18:34:45 +0300

    First right command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+import time 
 
 with open("keys/api.key") as f:
     api = f.read()
@@ -7,8 +8,18 @@ API_TOKEN = api
 
 bot = telebot.TeleBot(API_TOKEN)
 
+@bot.message_handler()
+def handle_dialog(message):
+    bot.send_message(message.chat.id, "Я пока не понимаю человеческую речь, только команды.")
+
+
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.send_message(message.chat.id, "Привет, я ваш ассистент July!\nЧем могу помочь?")
+
+@bot.message_handler(commands=['time'])
+def handle_time(message):
+    bot.send_message(message.chat.id, f"Точное время по Гринвичу: {time.time()}")
+
 
 bot.polling()
