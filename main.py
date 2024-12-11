ff2db125ab90899530299e0758208978f7da2057
arikallis-j <arikallis.j@gmail.com>
2024-12-11 18:36:23 +0300

    Fixed command

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ API_TOKEN = api
 
 bot = telebot.TeleBot(API_TOKEN)
 
-@bot.message_handler()
-def handle_dialog(message):
-    bot.send_message(message.chat.id, "Я пока не понимаю человеческую речь, только команды.")
-
-
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.send_message(message.chat.id, "Привет, я ваш ассистент July!\nЧем могу помочь?")
@@ -20,6 +15,11 @@ def handle_start(message):
 @bot.message_handler(commands=['time'])
 def handle_time(message):
     bot.send_message(message.chat.id, f"Точное время по Гринвичу: {time.time()}")
+
+
+@bot.message_handler()
+def handle_dialog(message):
+    bot.send_message(message.chat.id, "Я пока не понимаю человеческую речь, только команды.")
 
 
 bot.polling()
